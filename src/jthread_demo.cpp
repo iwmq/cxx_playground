d8960a5c3632242891a31f742444631157dd80b0
Jay Wang <iwmq@qq.com>
2021-07-02 05:12:40 +0800

    Using jthread

---
+++ src/jthread_demo.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<thread>
+
+using namespace std;
+
+void thread_func(int count)
+{
+    for(int i = 0; i < count; i++)
+    {
+        cout << "From spawned thread: i = " << i << endl;
+        this_thread::sleep_for(2000ms);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    cout << "Main thread started..." << endl;
+    
+    int count = 10;
+    jthread t(thread_func, count);
+
+    for(int i = 0; i < count; i++)
+    {
+        cout << "From main thread: i = " << i << endl;
+        this_thread::sleep_for(1000ms);
+    }
+
+    // t.join();
+
+    cout << "Main thread finished..." << endl;
+}
