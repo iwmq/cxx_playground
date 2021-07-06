b9d68243cd2c2c75dd575c770499f7f722972717
Jay Wang <iwmq@qq.com>
2021-07-06 22:51:37 +0800

    Add asio_timer_async.cpp

---
+++ asio/asio_timer_async.cpp
@@ -0,0 +1,29 @@
+#include<chrono>
+#include<ctime>
+#include<iostream>
+#include<boost/asio.hpp>
+#pragma comment(lib, "ws2_32")
+
+void timer_cb(const boost::system::error_code& /*e*/)
+{
+    std::cout << "Calling timer_cb!" << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    boost::asio::io_context io;
+
+    auto start = std::chrono::system_clock::now();
+    // Define an asynchronous timer
+    boost::asio::steady_timer t(io, std::chrono::seconds(5));
+    t.async_wait(timer_cb);
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end - start;
+
+    std::cout << "You just called a synchronous timer!" << std::endl;
+    std::cout << "Actual time used: " << diff.count() << " seconds!" << std::endl;
+
+    io.run();
+
+    return 0;
+}
